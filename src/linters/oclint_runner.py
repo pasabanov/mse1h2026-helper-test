829a6ce89859f890a1b01f933c7539661cfa25b3
Petr Sabanov <pasabanov@murena.io>
2026-04-24 20:09:54 +0300

    Тип вывода OCLint изменён с json на text

--- src/linters/oclint_runner.py
+++ src/linters/oclint_runner.py
@@ -8,19 +8,12 @@ class OCLintWrapper(Linter):
 	def run(self, file_path: str):
 		try:
 			result = subprocess.run(
-				['oclint','-report-type', 'json', file_path, '--', '-std=c++17', '-Wall'],
+				['oclint','-report-type', 'text', file_path, '--', '-std=c++17', '-Wall'],
 				capture_output=True,
 				text=True
 			)
-			if result.stdout:
-				try:
-					violations = json.loads(result.stdout)
-					return violations
-				except json.JSONDecodeError as e:
-					print(f"JSON decode error: {e}")
-					return result.stdout
-			elif result.stderr:
-				return result.stderr
-			return []
+			if result.stderr:
+				return result.stderr + '\n\n' + result.stdout
+			return result.stdout
 		except Exception as e:
 			return f"OCLint error: {str(e)}"
